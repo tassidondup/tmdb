1ffcb5cacff890a6c19c500ea9cd64980a2b70b5
Tashi Dhondup <tassidondup@gmail.com>
2021-10-15 17:39:10 +1100

    Styling of header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import './header.scss'
+import {Link, useLocation} from 'react-router-dom'
+import logo from '../../assets/tmovie.png'
+
+const headerNav = [
+  {
+    display: 'Home',
+    path: '/'
+  },
+  {
+    display: 'Movies',
+    path: '/movie'
+  },
+  {
+    display: 'TV Series',
+    path: '/tv'
+  }
+]
 
 const Header = () => {
+  const{pathname} = useLocation()
+  const headerRef = useRef(null)
+
+  const active = headerNav.findIndex(e => e.path === pathname)
+
   return (
-    <div>
-      Header
+    <div ref={headerRef} className='header'>
+      <div className="header__wrap container">
+        <div className="logo">
+          <img src={logo} alt="logo" />
+          <Link to='/'>tMovies</Link>
+        </div>
+        <ul className='header__nav'>
+          {headerNav.map((e, i) => (
+            <li key={i} className={`${i === active ? 'active' : ''}`}>
+              <Link to={e.path}>{e.display}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
