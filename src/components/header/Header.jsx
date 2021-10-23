9c2452aa24989ea881bbf9b8682a5249158fc10d
Tashi Dhondup <tassidondup@gmail.com>
2021-10-23 13:19:14 +1100

    styling of header as well as responsiveness and shrinking of header is done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react'
+import React, {useRef, useEffect } from 'react'
 import './header.scss'
 import {Link, useLocation} from 'react-router-dom'
 import logo from '../../assets/tmovie.png'
@@ -23,6 +23,21 @@ const Header = () => {
   const headerRef = useRef(null)
 
   const active = headerNav.findIndex(e => e.path === pathname)
+
+  useEffect(() => {
+    const shrinkHeader = () => {
+      if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        headerRef.current.classList.add('shrink')
+      } else {
+        headerRef.current.classList.remove('shrink')
+      }
+    }
+    
+    window.addEventListener('scroll', shrinkHeader)
+    return () => {
+      window.removeEventListener('scroll', shrinkHeader)
+    }
+  }, [])
 
   return (
     <div ref={headerRef} className='header'>
